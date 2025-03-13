377533521d18a1ca402be24fccbe2cf74a0b66d1
Thanh Sang Tran <vnthanhsang@gmail.com>
2025-03-13 20:49:24 +0700

    e

--- test1/test1/test1.cpp
+++ test1/test1/test1.cpp
@@ -1,196 +1,204 @@
-#include<iostream>
-#include<stdio.h>
+ï»¿#include<iostream>
 #include<ctime>
-#include<cstdio>
+#include<cstdlib>
 using namespace std;
 
-void TaoMangNgauNhien(int a[], int n)// T?o m?ng ng?u nhiên
-{
-	for (int i = 0; i < n; i++)
-		a[i] = rand() % 10000;
-}
-void InMang(int a[], int n)// In m?ng
-{
-	for (int i = 0; i < n; i++)
-		cout << a[i] << " ";
-	cout << endl;
-}
-void swap(int*a, int* b) //interchange sort
-{
-	int temp = *a;
-	*a = *b;
-	*b = temp;
+void TaoMangNgauNhien(int a[], int n) {
+    for (int i = 0; i < n; i++)
+        a[i] = rand() % 10000;
 }
 
-void bubbleSort(int a[], int n)// bubble sort
-{
-	for (int i = n - 1; i >= 1; i--) {
-		for (int j = 0; j < i ; j++) {
-			if (a[j] > a[j + 1])
-				swap(a[j], a[j + 1]);
-		}
-	}
-}
-void selectionSort(int a[], int n)// selection sort
-{
-	for (int i = 0; i < n - 1; i++) {
-		int min = i;
-		for (int j = i + 1; j < n; j++) {
-			if (a[j] < a[min])
-				min = j;
-		}
-		swap(a[i], a[min]);
-	}
-}
-void InsertionSort(int a[], int n)//Insertion Sort
-{
-	for (int i = 1; i < n; i++)
-	{
-		int j = i - 1;
-		int x = a[i];
-		bool cont = true;
-		while ((j >= 0) && cont)
-			if (a[j] > x)
-			{
-				a[j + 1] = a[j];
-				j--;
-			}
-			else
-				cont = false;
-		a[j + 1] = x;
-	}
+void InMang(int a[], int n) {
+    for (int i = 0; i < n; i++)
+        cout << a[i] << " ";
+    cout << endl;
 }
 
-void Partition(int a[], int p, int q) {
-	int x = a[(p + q) / 2];
-	int i = p;
-	int j = q;
-	do {
-		while (a[i] < x) i++;
-		while (a[j] > x) j--;
-		if (i <= j) {
-			swap(a[i], a[j]);
-			i++;
-			j--;
-		}
-	} while (i <= j);  
-	if (p < j)
-		Partition(a, p, j);
-	if (i < q)
-		Partition(a, i, q);
-}
-void QuickSort(int a[], int n)
-{
-	Partition(a, 0, n - 1);
-}
-void Merge(int a[], int temp[], int p, int t, int q)
-{
-	int i = p;
-	int k = t - 1;
-	int dem = 0;
-	while ((p <= k) && (t <= q))
-	{
-		if (a[p] < a[t])
-		{
-			temp[dem] = a[p];
-			p++;
-		}
-		else
-		{
-			temp[dem] = a[t];
-			t++;
-		}
-		dem++;
-	}
-	while (p <= k)
-	{
-		temp[dem] = a[p];
-		dem++;
-		p++;
-	}
-	while (t <= q)
-	{
-		temp[dem] = a[t];
-		dem++;
-		t++;
-	}
-	for (int j = 0; j < dem; j++)
-		a[i + j] = temp[j];
-}
-void RecMergeSort(int a[], int temp[], int p, int q)
-{
-	if (p < q)
-	{
-		int k = (p + q) / 2;
-		RecMergeSort(a, temp, p, k);
-		RecMergeSort(a, temp, k + 1, q);
-		Merge(a, temp, p, k + 1, q);
-	}
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
-void MergeSort(int a[], int n) {
-	int* temp = new int[n];
-	RecMergeSort(a, temp, 0, n - 1);
-	delete[] temp; 
+void bubbleSort(int a[], int n, int& sosanh, int& doi) {
+    for (int i = n - 1; i >= 1; i--) {
+        for (int j = 0; j < i; j++) {
+            sosanh++;
+            if (a[j] > a[j + 1]) {
+                swap(a[j], a[j + 1]);
+                doi++;
+            }
+        }
+    }
+}
+
+void selectionSort(int a[], int n, int& sosanh, int& doi) {
+    for (int i = 0; i < n - 1; i++) {
+        int min = i;
+        for (int j = i + 1; j < n; j++) {
+            sosanh++;
+            if (a[j] < a[min])
+                min = j;
+        }
+        if (min != i) {
+            swap(a[i], a[min]);
+            doi++;
+        }
+    }
+}
+
+void InsertionSort(int a[], int n, int& sosanh, int& doi) {
+    for (int i = 1; i < n; i++) {
+        int j = i - 1;
+        int x = a[i];
+        bool cont = true;
+        while ((j >= 0) && cont) {
+            sosanh++;
+            if (a[j] > x) {
+                a[j + 1] = a[j];
+                j--;
+                doi++;
+            }
+            else {
+                cont = false;
+            }
+        }
+        a[j + 1] = x;
+    }
+}
+
+void Partition(int a[], int p, int q, int& sosanh, int& doi) {
+    int x = a[(p + q) / 2];
+    int i = p;
+    int j = q;
+    do {
+        while (a[i] < x) { i++; sosanh++; }
+        while (a[j] > x) { j--; sosanh++; }
+        if (i <= j) {
+            swap(a[i], a[j]);
+            i++;
+            j--;
+            doi++;
+        }
+    } while (i <= j);
+    if (p < j)
+        Partition(a, p, j, sosanh, doi);
+    if (i < q)
+        Partition(a, i, q, sosanh, doi);
+}
+
+void QuickSort(int a[], int n, int& sosanh, int& doi) {
+    Partition(a, 0, n - 1, sosanh, doi);
+}
+
+void Merge(int a[], int temp[], int p, int t, int q, int& sosanh, int& doi) {
+    int i = p;
+    int k = t - 1;
+    int dem = 0;
+    while ((p <= k) && (t <= q)) {
+        sosanh++;
+        if (a[p] < a[t]) {
+            temp[dem] = a[p];
+            p++;
+        }
+        else {
+            temp[dem] = a[t];
+            t++;
+        }
+        dem++;
+    }
+    while (p <= k) {
+        temp[dem] = a[p];
+        dem++;
+        p++;
+    }
+    while (t <= q) {
+        temp[dem] = a[t];
+        dem++;
+        t++;
+    }
+    for (int j = 0; j < dem; j++) {
+        a[i + j] = temp[j];
+        doi++;
+    }
+}
+
+void RecMergeSort(int a[], int temp[], int p, int q, int& sosanh, int& doi) {
+    if (p < q) {
+        int k = (p + q) / 2;
+        RecMergeSort(a, temp, p, k, sosanh, doi);
+        RecMergeSort(a, temp, k + 1, q, sosanh, doi);
+        Merge(a, temp, p, k + 1, q, sosanh, doi);
+    }
+}
+
+void MergeSort(int a[], int n, int& sosanh, int& doi) {
+    int* temp = new int[n];
+    RecMergeSort(a, temp, 0, n - 1, sosanh, doi);
+    delete[] temp;
 }
 
 void test1() {
-	srand(time(NULL));
-	int z[] = { 10, 100, 200, 500, 1000, 2000, 5000, 10000 };
-	int t = 5; // s? l?n ch?y là 5
-	for (int i = 0; i < sizeof(z) / sizeof(z[0]); i++) {
-		int n = z[i];
-		int* a = new int[n];
-		clock_t start, end;
-		double cpu_time_used;
-		cout << "n = " << n << endl;
+    srand(time(NULL));
+    int z[] = { 10, 100, 200, 500, 1000, 2000, 5000, 10000 };
+    int t = 5; // sá»‘ láº§n cháº¡y lÃ  5
+    for (int i = 0; i < sizeof(z) / sizeof(z[0]); i++) {
+        int n = z[i];
+        int* a = new int[n];
+        clock_t start, end;
+        double time;
+        cout << "n = " << n << endl;
 
-		for (int j = 0; j < t; j++) {
-			TaoMangNgauNhien(a, n); // t?o m?ng ng?u nhiên
+        for (int j = 0; j < t; j++) {
+            int sosanh = 0, doi = 0;
 
-			start = clock();
-			bubbleSort(a, n); // bubble sort
-			end = clock();
-			cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC; // th?i gian ch?y
-			cout << "bubbleSort: " << cpu_time_used << " seconds" << endl;
+            TaoMangNgauNhien(a, n);
 
-			TaoMangNgauNhien(a, n);
-			start = clock();
-			selectionSort(a, n); // selection sort
-			end = clock();
-			cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC; // th?i gian ch?y
-			cout << "selectionSort: " << cpu_time_used << " seconds" << endl;
+            start = clock();
+            bubbleSort(a, n, sosanh, doi);
+            end = clock();
+            time = ((double)(end - start)) / CLOCKS_PER_SEC;
+            cout << "bubbleSort: Time = " << time << " giay, so sanh = " << sosanh << ", doi = " << doi << endl;
+            sosanh = 0; doi = 0;
 
-			TaoMangNgauNhien(a, n);
-			start = clock();
-			InsertionSort(a, n); // Insertion Sort
-			end = clock();
-			cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC; // th?i gian ch?y
-			cout << "InsertionSort: " << cpu_time_used << " seconds" << endl;
+            TaoMangNgauNhien(a, n);
+            start = clock();
+            selectionSort(a, n, sosanh, doi);
+            end = clock();
+            time = ((double)(end - start)) / CLOCKS_PER_SEC;
+            cout << "selectionSort: Time = " << time << " giay, so sanh = " << sosanh << ", doi = " << doi << endl;
+            sosanh = 0; doi = 0;
 
-			TaoMangNgauNhien(a, n);
-			start = clock();
-			QuickSort(a, n); // Quick Sort
-			end = clock();
-			cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC; // th?i gian ch?y
-			cout << "QuickSort: " << cpu_time_used << " seconds" << endl;
+            TaoMangNgauNhien(a, n);
+            start = clock();
+            InsertionSort(a, n, sosanh, doi);
+            end = clock();
+            time = ((double)(end - start)) / CLOCKS_PER_SEC;
+            cout << "InsertionSort: Time = " << time << " giay, so sanh = " << sosanh << ", doi = " << doi << endl;
+            sosanh = 0; doi = 0;
 
-			TaoMangNgauNhien(a, n);
-			start = clock();
-			MergeSort(a, n); // Merge Sort
-			end = clock();
-			cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC; // th?i gian ch?y
-			cout << "MergeSort: " << cpu_time_used << " seconds" << endl;
+            TaoMangNgauNhien(a, n);
+            start = clock();
+            QuickSort(a, n, sosanh, doi);
+            end = clock();
+            time = ((double)(end - start)) / CLOCKS_PER_SEC;
+            cout << "QuickSort: Time = " << time << " giay, so sanh = " << sosanh << ", doi = " << doi << endl;
+            sosanh = 0; doi = 0;
 
-			cout << endl;
-		}
+            TaoMangNgauNhien(a, n);
+            start = clock();
+            MergeSort(a, n, sosanh, doi);
+            end = clock();
+            time = ((double)(end - start)) / CLOCKS_PER_SEC;
+            cout << "MergeSort: Time = " << time << " giay, so sanh = " << sosanh << ", doi = " << doi << endl;
+            cout << endl;
+        }
 
-		delete[] a; 
-	}
+        delete[] a;
+    }
 }
 
-int main()
-{
-	test1();
-	return 0;
+int main() {
+    test1();
+    return 0;
 }
---
+++ test2/test2/test2.cpp
@@ -0,0 +1,303 @@
+﻿#include<iostream>
+#include<ctime>
+#include<cstdlib>
+#include<fstream>
+using namespace std;
+void DocMangTuFile(const string& TextFile1, int a[], int n) {
+    ifstream file(TextFile1);
+    for (int i = 0; i < n; i++) {
+        file >> a[i];
+    }
+    file.close();
+}
+void GhiMangVaoFile(const string& TextFile1, int a[], int n) {
+    ofstream file(TextFile1);
+    for (int i = 0; i < n; i++) {
+        file << a[i] << " ";
+    }
+    file.close();
+}
+void TaoMangNgauNhien(int a[], int n) {
+    for (int i = 0; i < n; i++)
+        a[i] = rand() % 10000;
+}
+
+void InMang(int a[], int n) {
+    for (int i = 0; i < n; i++)
+        cout << a[i] << " ";
+    cout << endl;
+}
+
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+void interchangeSort(int a[], int n, int& sosanh, int& doi) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            sosanh++;
+            if (a[i] > a[j]) {
+                swap(a[i], a[j]);
+                doi++;
+            }
+        }
+    }
+}
+
+void bubbleSort(int a[], int n, int& sosanh, int& doi) {
+    for (int i = n - 1; i >= 1; i--) {
+        for (int j = 0; j < i; j++) {
+            sosanh++;
+            if (a[j] > a[j + 1]) {
+                swap(a[j], a[j + 1]);
+                doi++;
+            }
+        }
+    }
+}
+
+void selectionSort(int a[], int n, int& sosanh, int& doi) {
+    for (int i = 0; i < n - 1; i++) {
+        int min = i;
+        for (int j = i + 1; j < n; j++) {
+            sosanh++;
+            if (a[j] < a[min])
+                min = j;
+        }
+        if (min != i) {
+            swap(a[i], a[min]);
+            doi++;
+        }
+    }
+}
+
+void InsertionSort(int a[], int n, int& sosanh, int& doi) {
+    for (int i = 1; i < n; i++) {
+        int j = i - 1;
+        int x = a[i];
+        bool cont = true;
+        while ((j >= 0) && cont) {
+            sosanh++;
+            if (a[j] > x) {
+                a[j + 1] = a[j];
+                j--;
+                doi++;
+            }
+            else {
+                cont = false;
+            }
+        }
+        a[j + 1] = x;
+    }
+}
+
+void Partition(int a[], int p, int q, int& sosanh, int& doi) {
+    int x = a[(p + q) / 2];
+    int i = p;
+    int j = q;
+    do {
+        while (a[i] < x) { i++; sosanh++; }
+        while (a[j] > x) { j--; sosanh++; }
+        if (i <= j) {
+            swap(a[i], a[j]);
+            i++;
+            j--;
+            doi++;
+        }
+    } while (i <= j);
+    if (p < j)
+        Partition(a, p, j, sosanh, doi);
+    if (i < q)
+        Partition(a, i, q, sosanh, doi);
+}
+
+void QuickSort(int a[], int n, int& sosanh, int& doi) {
+    Partition(a, 0, n - 1, sosanh, doi);
+}
+
+void Merge(int a[], int temp[], int p, int t, int q, int& sosanh, int& doi) {
+    int i = p;
+    int k = t - 1;
+    int dem = 0;
+    while ((p <= k) && (t <= q)) {
+        sosanh++;
+        if (a[p] < a[t]) {
+            temp[dem] = a[p];
+            p++;
+        }
+        else {
+            temp[dem] = a[t];
+            t++;
+        }
+        dem++;
+    }
+    while (p <= k) {
+        temp[dem] = a[p];
+        dem++;
+        p++;
+    }
+    while (t <= q) {
+        temp[dem] = a[t];
+        dem++;
+        t++;
+    }
+    for (int j = 0; j < dem; j++) {
+        a[i + j] = temp[j];
+        doi++;
+    }
+}
+
+void RecMergeSort(int a[], int temp[], int p, int q, int& sosanh, int& doi) {
+    if (p < q) {
+        int k = (p + q) / 2;
+        RecMergeSort(a, temp, p, k, sosanh, doi);
+        RecMergeSort(a, temp, k + 1, q, sosanh, doi);
+        Merge(a, temp, p, k + 1, q, sosanh, doi);
+    }
+}
+
+void MergeSort(int a[], int n, int& sosanh, int& doi) {
+    int* temp = new int[n];
+    RecMergeSort(a, temp, 0, n - 1, sosanh, doi);
+    delete[] temp;
+}
+void taoheap(int a[], int n, int i, int& sosanh, int& doi) {
+    int lonnhat = i;
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
+
+    if (l < n) {
+        sosanh++;
+        if (a[l] > a[lonnhat])
+            lonnhat = l;
+    }
+
+    if (r < n) {
+        sosanh++;
+        if (a[r] > a[lonnhat])
+            lonnhat = r;
+    }
+
+    if (lonnhat != i) {
+        swap(a[i], a[lonnhat]);
+        doi++;
+        taoheap(a, n, lonnhat, sosanh, doi);
+    }
+}
+
+void heapSort(int a[], int n, int& sosanh, int& doi) {
+    for (int i = n / 2 - 1; i >= 0; i--)
+        taoheap(a, n, i, sosanh, doi);
+
+    for (int i = n - 1; i > 0; i--) {
+        swap(a[0], a[i]);
+        doi++;
+        taoheap(a, i, 0, sosanh, doi);
+    }
+}
+
+void test1() {
+    srand(time(NULL));
+    const int n = 30000;
+    int* a = new int[n];
+    int* temp = new int[n]; // Mảng tạm để sao chép dữ liệu
+    clock_t start, end;
+    double time;
+    int sosanh = 0, doi = 0;
+
+    // Tạo mảng ngẫu nhiên và lưu vào file
+    TaoMangNgauNhien(a, n);
+    GhiMangVaoFile("TextFile1.txt", a, n);
+
+    
+    DocMangTuFile("TextFile1.txt", a, n);
+
+    // Interchange Sort
+    start = clock();
+    interchangeSort(a, n, sosanh, doi);
+    end = clock();
+    time = ((double)(end - start)) / CLOCKS_PER_SEC;
+    cout << "Interchange Sort: Time = " << time << " giay, so sanh = " << sosanh << ", doi = " << doi << endl;
+    GhiMangVaoFile("TextFile2", a, n);
+    sosanh = 0; doi = 0;
+
+    
+    DocMangTuFile("TextFile1.txt", a, n);
+
+    // Selection Sort
+    start = clock();
+    selectionSort(a, n, sosanh, doi);
+    end = clock();
+    time = ((double)(end - start)) / CLOCKS_PER_SEC;
+    cout << "Selection Sort: Time = " << time << " giay, so sanh = " << sosanh << ", doi = " << doi << endl;
+    GhiMangVaoFile("TextFile2", a, n);
+    sosanh = 0; doi = 0;
+
+    
+    DocMangTuFile("TextFile1.txt", a, n);
+
+    // Insertion Sort
+    start = clock();
+    InsertionSort(a, n, sosanh, doi);
+    end = clock();
+    time = ((double)(end - start)) / CLOCKS_PER_SEC;
+    cout << "Insertion Sort: Time = " << time << " giay, so sanh = " << sosanh << ", doi = " << doi << endl;
+    GhiMangVaoFile("TextFile2", a, n);
+    sosanh = 0; doi = 0;
+
+    
+    DocMangTuFile("TextFile1.txt", a, n);
+
+    // Bubble Sort
+    start = clock();
+    bubbleSort(a, n, sosanh, doi);
+    end = clock();
+    time = ((double)(end - start)) / CLOCKS_PER_SEC;
+    cout << "Bubble Sort: Time = " << time << " giay, so sanh = " << sosanh << ", doi = " << doi << endl;
+    GhiMangVaoFile("TextFile2", a, n);
+    sosanh = 0; doi = 0;
+
+    
+    DocMangTuFile("TextFile1.txt", a, n);
+
+    // Quick Sort
+    start = clock();
+    QuickSort(a, n, sosanh, doi);
+    end = clock();
+    time = ((double)(end - start)) / CLOCKS_PER_SEC;
+    cout << "Quick Sort: Time = " << time << " giay, so sanh = " << sosanh << ", doi = " << doi << endl;
+    GhiMangVaoFile("TextFile2", a, n);
+    sosanh = 0; doi = 0;
+
+    // Đọc lại dữ liệu từ file
+    DocMangTuFile("input.txt", a, n);
+
+    // Merge Sort
+    start = clock();
+    MergeSort(a, n, sosanh, doi);
+    end = clock();
+    time = ((double)(end - start)) / CLOCKS_PER_SEC;
+    cout << "Merge Sort: Time = " << time << " giay, so sanh = " << sosanh << ", doi = " << doi << endl;
+    GhiMangVaoFile("TextFile2", a, n);
+    sosanh = 0; doi = 0;
+
+    // Đọc lại dữ liệu từ file
+    DocMangTuFile("TextFile1.txt", a, n);
+
+    // Heap Sort
+    start = clock();
+    heapSort(a, n, sosanh, doi);
+    end = clock();
+    time = ((double)(end - start)) / CLOCKS_PER_SEC;
+    cout << "Heap Sort: Time = " << time << " giay, so sanh = " << sosanh << ", doi = " << doi << endl;
+    GhiMangVaoFile("TextFile2", a, n);
+
+    delete[] a;
+    delete[] temp;
+}
+int main()
+{
+    test1();
+    return 0;
+}
