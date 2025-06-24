5575246d3cb0f8841a5187fcf915501d137935cc
tsangdrowsy <vnthanhsang@gmail.com>
2025-06-24 08:29:15 +0700

    Update PageReplace.cpp

--- PageReplace/PageReplace/PageReplace.cpp
+++ PageReplace/PageReplace/PageReplace.cpp
@@ -5,6 +5,7 @@
 #include <unordered_set>
 #include <queue>
 #include <unordered_map>
+#include <iomanip>
 
 using namespace std;
 
@@ -25,7 +26,7 @@ void readPageReplacementInput(const string& filename, vector<int>& pages, int& f
 }
 
 void writePageReplacementOutput(const string& filename, const vector<int>& pageFaults,
-    const string& algorithm, int frameCount) {
+    const string& algorithm, int frameCount, const vector<vector<int>>& framesState) {
     ofstream out(filename);
     if (!out.is_open()) {
         cerr << "Khong the tao file " << filename << endl;
@@ -42,15 +43,36 @@ void writePageReplacementOutput(const string& filename, const vector<int>& pageF
             out << ", ";
         }
     }
-    out << "\n";
+    out << "\n\nBang thay the trang:\n";
+
+    out << "Buoc\t";
+    for (int i = 0; i < frameCount; i++) {
+        out << "Frame " << i << "\t";
+    }
+    out << "Page Fault\n";
+
+    for (size_t i = 0; i < framesState.size(); i++) {
+        out << i + 1 << "\t";
+        for (int j = 0; j < frameCount; j++) {
+            if (j < framesState[i].size()) {
+                out << framesState[i][j] << "\t";
+            }
+            else {
+                out << "-\t";
+            }
+        }
+        bool isFault = find(pageFaults.begin(), pageFaults.end(), i) != pageFaults.end();
+        out << (isFault ? "x" : "") << "\n";
+    }
 }
 
-vector<int> fifoPageReplacement(const vector<int>& pages, int frameCount) {
+vector<int> fifoPageReplacement(const vector<int>& pages, int frameCount, vector<vector<int>>& framesState) {
     unordered_set<int> frames;
     queue<int> indexes;
     vector<int> pageFaults;
 
     for (size_t i = 0; i < pages.size(); i++) {
+        vector<int> currentState;
         if (frames.size() < frameCount) {
             if (frames.find(pages[i]) == frames.end()) {
                 frames.insert(pages[i]);
@@ -68,17 +90,25 @@ vector<int> fifoPageReplacement(const vector<int>& pages, int frameCount) {
                 pageFaults.push_back(i);
             }
         }
+
+        queue<int> temp = indexes;
+        while (!temp.empty()) {
+            currentState.push_back(temp.front());
+            temp.pop();
+        }
+        framesState.push_back(currentState);
     }
     return pageFaults;
 }
 
-vector<int> lruPageReplacement(const vector<int>& pages, int frameCount) {
+vector<int> lruPageReplacement(const vector<int>& pages, int frameCount, vector<vector<int>>& framesState) {
     unordered_set<int> frames;
     unordered_map<int, int> indexes;
     vector<int> pageFaults;
     int time = 0;
 
     for (size_t i = 0; i < pages.size(); i++) {
+        vector<int> currentState;
         if (frames.size() < frameCount) {
             if (frames.find(pages[i]) == frames.end()) {
                 frames.insert(pages[i]);
@@ -101,6 +131,17 @@ vector<int> lruPageReplacement(const vector<int>& pages, int frameCount) {
             }
             indexes[pages[i]] = time++;
         }
+
+        vector<pair<int, int>> sortedFrames;
+        for (auto page : frames) {
+            sortedFrames.push_back({ indexes[page], page });
+        }
+        sort(sortedFrames.begin(), sortedFrames.end());
+
+        for (auto& p : sortedFrames) {
+            currentState.push_back(p.second);
+        }
+        framesState.push_back(currentState);
     }
     return pageFaults;
 }
@@ -111,15 +152,17 @@ void pageReplacement() {
 
     readPageReplacementInput("input.txt", pages, frameCount);
 
-    vector<int> fifoFaults = fifoPageReplacement(pages, frameCount);
-    writePageReplacementOutput("fifo_output.txt", fifoFaults, "FIFO", frameCount);
+    vector<vector<int>> fifoFramesState;
+    vector<int> fifoFaults = fifoPageReplacement(pages, frameCount, fifoFramesState);
+    writePageReplacementOutput("fifo_output.txt", fifoFaults, "FIFO", frameCount, fifoFramesState);
 
-    vector<int> lruFaults = lruPageReplacement(pages, frameCount);
-    writePageReplacementOutput("lru_output.txt", lruFaults, "LRU", frameCount);
+    vector<vector<int>> lruFramesState;
+    vector<int> lruFaults = lruPageReplacement(pages, frameCount, lruFramesState);
+    writePageReplacementOutput("lru_output.txt", lruFaults, "LRU", frameCount, lruFramesState);
 }
 
 int main() {
-	pageReplacement();
-	cout << "Da hoan thanh viec thay the trang." << endl;
-	return 0;
+    pageReplacement();
+    cout << "Da hoan thanh viec thay the trang." << endl;
+    return 0;
 }
