31ac4c2c63cb254ec4f8e884f144969b0ad571cc
drowsy <vnthanhsang@gmail.com>
2026-09-22 21:45:36 +0700

    ea sports

---
+++ CSharp/Tuan2/Bai2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Bai1
+{
+    class NhapTen
+    {
+        public static void Main(String[] args)
+        {
+
+
+            Console.Write("Nhap ho ten cua ban: ");
+            string e = Console.ReadLine();
+
+            Console.Write("Chao ban "+e);
+
+
+        }
+    }
+}
