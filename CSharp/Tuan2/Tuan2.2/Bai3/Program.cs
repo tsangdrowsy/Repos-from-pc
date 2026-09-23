59816c1c54052389b49b481c06eec30bf5f4eb42
drowsy <vnthanhsang@gmail.com>
2026-09-23 23:13:10 +0700

    done this class (didn't tell me to do main so I'd rather not)

---
+++ CSharp/Tuan2/Tuan2.2/Bai3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Bai3
+{
+    public class Person
+    {
+        // Các dữ liệu
+        public string id;
+        public string name;
+        public int yob;
+        public int yod;
+
+       public Person()
+        {
+            id = "";
+            name = "";
+            yob = 0;
+            yod = 0;
+        }
+
+         public Person(Person other) //copy
+        {
+            if (other != null)
+            {
+                this.id = other.id;
+                this.name = other.name;
+                this.yob = other.yob;
+                this.yod = other.yod;
+            }
+        }
+
+         public Person(string id, string name, int yob, int yod) //input yes
+        {
+            this.id = id;
+            this.name = name;
+            this.yob = yob;
+            this.yod = yod;
+        }
+
+         public bool IsLiving()
+        {
+            return yod == 0;
+        }
+    }
+}
